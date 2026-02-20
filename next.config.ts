f1ff4cf047b950a6feb7089564838651d926a348
Lucas <lucasdegoes@outlook.com.br>
2026-02-20 03:30:05 -0300

    fix puppeteer types denovo

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from "next";
-
 const nextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
     },
   },
+
+  // 🔥 IMPORTANTE PRA PUPPETEER
+  serverExternalPackages: ["@sparticuz/chromium"],
 };
 
 export default nextConfig;
-
